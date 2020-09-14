b55d832adc57868a87a554ac4610388e0d4e8a29
Samuel Mann <samuelcmann@gmail.com>
2020-09-14 08:34:36 +0200

    test config

--- src/components/LandingPage/TitleHeader.js
+++ src/components/LandingPage/TitleHeader.js
@@ -3,6 +3,6 @@ import React from 'react'
 export const TitleHeader = () => (
   <div className="text-center">
     <h1 className="text-5xl p-8 md:pt-16 md:pb-8">Hi, I'm Moni 👋</h1>
-    <h3 className="text-xl">Welcome to my blog.</h3>
+    <h3 className="text-xl">Welcome to my blog!</h3>
   </div>
 )
